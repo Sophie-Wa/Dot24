5106b84fcec47359549af5820d5119e332e86ba9
Sophie-Wa <2778812430@qq.com>
2022-06-13 01:02:05 +0800

    提交

--- src/main/java/com/example/dot24/HelloApplication.java
+++ src/main/java/com/example/dot24/HelloApplication.java
@@ -252,8 +252,6 @@ public class HelloApplication extends Application {
         int t=random.nextInt(3)+2;
         String name=new String();
         while(i++<t){
-
-
             highCode = (176 + Math.abs(random.nextInt(39))); //B0 + 0~39(16~55) 一级汉字所占区
             lowCode = (161 + Math.abs(random.nextInt(93))); //A1 + 0~93 每区有94个汉字
             byte[] b = new byte[2];
